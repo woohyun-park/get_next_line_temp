98c95f7623907e1a6298ff190b9696d121c78e72
Woohyun Park <woohyunpark.mail@gmail.com>
2021-06-08 09:29:27 +0000

    MODIFY, modify to pass 42TESTERS-GNL

--- get_next_line.c
+++ get_next_line.c
@@ -16,23 +16,45 @@ int index_of(char *str, char c)
 
 int	assign_line(char **from, char **to, int size)
 {
-	if(!(*to = malloc(size + 1)))
-		return (-1);
-	ft_strlcpy(*to, *from, size + 1);
-	*from = size == BUFFER_SIZE ? *from + size : *from + size + 1;
-	return (1);
-}
-
-int	assign_line_last(char **from, char **to, int size)
-{
-	if(size <= 0 && ft_strlen(*from) <= BUFFER_SIZE){
-		ft_strlcpy(*to, *from, ft_strlen(*from) + 1);
-		return (0);
+	// if(size == 0)
+	// {
+	// 	ft_strlcpy(*to, "", 0);
+	// 	*from = *from + 1;
+	// 	return (1);
+	// }
+	if(size == 0)
+	{
+		*to = malloc(1);
+		*to[0] = 0;
+		*from = *from + 1;
+		return (1);
 	}
 	if(!(*to = malloc(size + 1)))
 		return (-1);
 	ft_strlcpy(*to, *from, size + 1);
-	*from = size == BUFFER_SIZE ? *from + size : *from + size + 1;
+	*from = *from + size + 1;
+	return (1);
+}
+
+int	assign_line_last(char **from, char **to, int size)
+{	
+	if(size == 0)
+	{
+		*to = malloc(1);
+		*to[0] = 0;
+		// printf("\n:BEFORE:%s:\n", *from);
+		if(ft_strlen(*from) == 0)
+			return (0);
+		*from = *from + 1;
+		// printf("\n:AFTER:%s:\n", *from);
+		return (1);
+	}
+	if(!(*to = malloc(size + 1)))
+		return (-1);
+	ft_strlcpy(*to, *from, size + 1);
+	*from = *from + size + 1;
+	if(ft_strlen(*from) == 0)
+		return (0);
 	return (1);
 }
 
@@ -42,11 +64,12 @@ int	get_next_line(int fd, char **line)
 	char		*buf;
 	int			index;
 	int			size_read;
-	
-	if (fd < 0 || line == 0 || BUFFER_SIZE <= 0)
+
+	if (fd < 0 || fd > OPEN_MAX || line == 0 || BUFFER_SIZE <= 0)
 		return (-1);
 	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (-1);
+	// printf("\n?%s?\n", buf_save[fd]);
 	while ((size_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[size_read] = 0;
@@ -54,25 +77,27 @@ int	get_next_line(int fd, char **line)
 		if((index = index_of(buf_save[fd], '\n')) != -1)
 			return (assign_line(&buf_save[fd], line, index));
 	}
+	if((index = index_of(buf_save[fd], '\n')) >= 0)
+		return (assign_line_last(&buf_save[fd], line, index));
 	return assign_line_last(&buf_save[fd], line, ft_strlen(buf_save[fd]));
 }
 
-int	main(void)
-{
-	int		temp;
-	int		fd;
-	char	*line;
+// int	main(void)
+// {
+// 	int		temp;
+// 	int		fd;
+// 	char	*line;
 
-	fd = open("test.txt", O_RDONLY);
-	while ((temp = (get_next_line(fd, &line)) > 0))
-	{
-		printf("%s\n", line);
-		free(line);
-	}
-	printf("%s\n", line);
-	free(line);
-	close(fd);
+// 	fd = open("test.txt", O_RDONLY);
+// 	while ((temp = (get_next_line(fd, &line)) > 0))
+// 	{
+// 		printf("|%s\n", line);
+// 		free(line);
+// 	}
+// 	printf("|%s\n", line);
+// 	free(line);
+// 	close(fd);
 	
-	system("leaks a.out");
-	return (0);
-}
+// 	system("leaks a.out");
+// 	return (0);
+// }
