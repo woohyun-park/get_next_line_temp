7980bc7f3d6cacb377d6123f413a8a74fa2e8448
woopark <woopark@student.42seoul.kr>
2022-01-14 14:35:46 +0900

    TESTER works

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: woopark <woopark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 12:24:07 by woopark           #+#    #+#             */
-/*   Updated: 2022/01/14 14:00:34 by woopark          ###   ########.fr       */
+/*   Updated: 2022/01/14 14:34:40 by woopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,36 +31,22 @@ int		is_line(char *str)
 
 char	*create_next_line(char **save, int index)
 {
-	// char	*tmp;
-	// char	*tmp2;
-	// char	*result;
-
-	// result = (char *) malloc(sizeof(char) * index + 1);
-	// ft_strlcpy(result, *save, index + 1);
-	// tmp2 = ft_strjoin(result, "\n");
-	// free(result);
-	// result = tmp2;
-	// if (* (*save + index + 1) == 0)
-	// 	tmp = 0;
-	// else
-	// 	tmp = ft_strdup((*save) + index + 1);
-	// free(*save);
-	// *save = tmp;
-	// return result;
-	
 	char *result;
 	char *tmp_result;
 	char *tmp;
 
 	(*save)[index] = 0;
 	result = ft_strdup(*save);
+	// printf("result: !%s!\n", result);
 	tmp_result = ft_strjoin(result, "\n");
 	free(result);
 	result = tmp_result;
+	// printf("result: !%s!\n", result);
 	if (*(*save + index + 1) == '\0')
 		tmp = 0;
 	else
 		tmp = ft_strdup((*save) + index + 1);
+	// printf("tmp: !%s!\n", tmp);
 	free(*save);
 	*save = tmp;
 	return result;
@@ -70,7 +56,7 @@ char	*create_last_line(char **save, int size)
 {
 	int	index;
 	char *result;
-	char	*tmp;
+	// char	*tmp;
 
 	if (size < 0)
 		return (0);
@@ -83,11 +69,7 @@ char	*create_last_line(char **save, int size)
 		*save = 0;
 		return (result);
 	}
-	result = ft_strdup(*save);
-	tmp = ft_strjoin(result, "\n");
-	free(result);
-	result = tmp;
-	return result;
+	return create_next_line(save, index);
 }
 
 char	*get_next_line(int fd)
@@ -96,6 +78,7 @@ char	*get_next_line(int fd)
 	char		buf[BUFFER_SIZE + 1];
 	int			index;
 	int			size_read;
+	char		*tmp;
 
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
 		return (0);
@@ -103,8 +86,9 @@ char	*get_next_line(int fd)
 	while (size_read > 0)
 	{
 		buf[size_read] = 0;
+		tmp = ft_strjoin(save[fd], buf);
 		free(save[fd]);
-		save[fd] = ft_strjoin(save[fd], buf);
+		save[fd] = tmp;
 		index = is_line(save[fd]);
 		if (index >= 0)
 			return create_next_line(&save[fd], index);
@@ -115,7 +99,7 @@ char	*get_next_line(int fd)
 
 // int main(){
 // 	int fd;
-// 	fd = open("gnlTester/files/nl", O_RDWR);
+// 	fd = open("gnlTester/files/multiple_nlx5", O_RDWR);
 // 	printf(":%s:", get_next_line(fd));
 // 	printf(":%s:", get_next_line(fd));
 // }
