788016c49e622efafa1728835ca231264cfe564e
woopark <woopark@student.42seoul.kr>
2022-01-14 14:02:37 +0900

    multiple, alternate, big, stdin needed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: woopark <woopark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 12:24:07 by woopark           #+#    #+#             */
-/*   Updated: 2022/01/14 13:44:47 by woopark          ###   ########.fr       */
+/*   Updated: 2022/01/14 14:00:34 by woopark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,33 @@ int		is_line(char *str)
 
 char	*create_next_line(char **save, int index)
 {
-	char	*tmp;
-	char	*tmp2;
-	char	*result;
+	// char	*tmp;
+	// char	*tmp2;
+	// char	*result;
 
-	result = (char *) malloc(sizeof(char) * index + 1);
-	ft_strlcpy(result, *save, index + 1);
-	tmp2 = ft_strjoin(result, "\n");
-	free(result);
-	result = tmp2;
+	// result = (char *) malloc(sizeof(char) * index + 1);
+	// ft_strlcpy(result, *save, index + 1);
+	// tmp2 = ft_strjoin(result, "\n");
+	// free(result);
+	// result = tmp2;
+	// if (* (*save + index + 1) == 0)
+	// 	tmp = 0;
+	// else
+	// 	tmp = ft_strdup((*save) + index + 1);
+	// free(*save);
+	// *save = tmp;
+	// return result;
 	
-	// (*save)[index + 1] = 0;
-	// (*save)[index] = '\n';
-	// result = ft_strdup(*save);
-	if (* (*save + index + 1) == 0)
+	char *result;
+	char *tmp_result;
+	char *tmp;
+
+	(*save)[index] = 0;
+	result = ft_strdup(*save);
+	tmp_result = ft_strjoin(result, "\n");
+	free(result);
+	result = tmp_result;
+	if (*(*save + index + 1) == '\0')
 		tmp = 0;
 	else
 		tmp = ft_strdup((*save) + index + 1);
@@ -70,14 +83,11 @@ char	*create_last_line(char **save, int size)
 		*save = 0;
 		return (result);
 	}
-
 	result = ft_strdup(*save);
 	tmp = ft_strjoin(result, "\n");
 	free(result);
 	result = tmp;
 	return result;
-
-	// return create_next_line(save, index);
 }
 
 char	*get_next_line(int fd)
@@ -105,8 +115,7 @@ char	*get_next_line(int fd)
 
 // int main(){
 // 	int fd;
-// 	fd = open("41_with_nl", O_RDWR);
-// 	printf(":%s:", get_next_line(fd));
+// 	fd = open("gnlTester/files/nl", O_RDWR);
 // 	printf(":%s:", get_next_line(fd));
 // 	printf(":%s:", get_next_line(fd));
 // }
